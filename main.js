a41cd9191300ee42903ad42b6f5204e1457cbcb0
Keith Potempa <keithrpotempa@gmail.com>
2020-01-17 14:28:50 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,27 @@
+// Your job is to define the different objects and arrays, 
+// their structure, and the corresponding properties for each, 
+// to represent the following information about Elizabeth's campaign.
+
+// Her congressional district (you can use yours here)
+// Her platform statements for the following issues.
+// Taxes
+// Jobs
+// Infrastructure
+// Health care
+// Crime and enforcement
+// URL for donation form
+// Calendar of events
+// Volunteer information
+// Name
+// Address
+// Email
+// Phone number
+// Availability
+// What activities each one is willing to do (e.g. answering phone calls, taking polls, etc.)
+// Biography
+// Image gallery
+// Head shot
+// Picture of family
+// Picture of constituents
+// Mission statement
+// URL for registering to vote
